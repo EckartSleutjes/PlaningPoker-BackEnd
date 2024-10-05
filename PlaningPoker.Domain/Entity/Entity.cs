3d3e5ca33ff37676966e070896878bf1dc082ece
Marcos Eckart <Marcos Eckart@ECKART>
2024-10-05 11:49:06 -0300

    create entity room + entity

---
+++ PlaningPoker.Domain/Entity/Entity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlaningPoker.Domain.Entity
+{
+    public class Entity
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public Guid CreatedBy { get; set; }
+    }
+}
---
+++ PlaningPoker.Domain/Entity/Room.cs
@@ -0,0 +1,10 @@
+﻿namespace PlaningPoker.Domain.Entity
+{
+    public class Room : Entity
+    {
+        public string Tag { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string? Password { get; set; } 
+    }
+}
