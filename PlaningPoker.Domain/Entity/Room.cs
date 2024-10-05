20a0a43b39ce71e38d1016970a8ed7d67f51ea25
Marcos Eckart <Marcos Eckart@ECKART>
2024-10-05 12:00:27 -0300

    add createdBy in room entity

--- PlaningPoker.Domain/Entity/Room.cs
+++ PlaningPoker.Domain/Entity/Room.cs
@@ -3,12 +3,13 @@
     public class Room : Entity
     {
         private Room() { }
-        public Room(string tag, string name, string? description = null, string? password = null)
+        public Room(string tag, string name, Guid createdBy, string? description = null, string? password = null)
         {
             Tag = tag;
             Name = name;
             Description = description;
             Password = password;
+            CreatedBy = createdBy;
         }
         public string Tag { get; set; }
         public string Name { get; set; }
